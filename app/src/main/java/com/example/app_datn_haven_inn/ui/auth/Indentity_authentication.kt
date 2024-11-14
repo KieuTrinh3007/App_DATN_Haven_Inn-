7d2e09abca906a94b2c4f1270c59b9b323edf3b2
Vanh2k20 <avan81837@gmail.com>
2024-11-14 19:09:41 +0700

    auth

---
+++ app/src/main/java/com/example/app_datn_haven_inn/ui/auth/Indentity_authentication.kt
@@ -0,0 +1,21 @@
+package com.example.app_datn_haven_inn.ui.auth
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.app_datn_haven_inn.R
+
+class Indentity_authentication : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		enableEdgeToEdge()
+		setContentView(R.layout.activity_indentity_authentication)
+		ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+			val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+			insets
+		}
+	}
+}
