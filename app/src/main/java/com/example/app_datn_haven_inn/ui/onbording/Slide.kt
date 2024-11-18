15bb6a0f76c8ef2f5e5c450fe6786b7484a901af
HieuGitHub28 <phamhieu28042004@gmail.com>
2024-11-18 15:39:59 +0700

    ui/ onbording

---
+++ app/src/main/java/com/example/app_datn_haven_inn/ui/onbording/Slide.kt
@@ -0,0 +1,4 @@
+package com.example.app_datn_haven_inn.ui.onbording
+
+class Slide {
+}
--- app/src/main/java/com/example/app_datn_haven_inn/ui/thucDon/ThucDonFragment.kt
+++ app/src/main/java/com/example/app_datn_haven_inn/ui/thucDon/ThucDonFragment.kt
@@ -12,11 +12,6 @@ import com.example.app_datn_haven_inn.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ThucDonFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ThucDonFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
