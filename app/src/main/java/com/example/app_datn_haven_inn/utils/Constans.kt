bf8fa773914e5389fc130926b29a4d23b4432999
HOA QUANG LINH <linhvnmcc@gmail.com>
2024-12-12 18:40:12 +0700

    fix loi thanh toan, cccd

--- app/src/main/java/com/example/app_datn_haven_inn/utils/Constans.kt
+++ app/src/main/java/com/example/app_datn_haven_inn/utils/Constans.kt
@@ -1,5 +1,5 @@
 package com.example.app_datn_haven_inn.utils
 
 object Constans {
-    const val DOMAIN = "https://haven-inn.onrender.com/api/"
+    const val DOMAIN = "http://192.168.0.107:3000/api/"
 }
