7feae225cd185ce9bf5a6a007555d37ca0a0c296
Vanh2k20 <avan81837@gmail.com>
2024-12-14 14:50:39 +0700

    fix ho tro

--- app/src/main/java/com/example/app_datn_haven_inn/ui/myRoom/MyRoomActivity.kt
+++ app/src/main/java/com/example/app_datn_haven_inn/ui/myRoom/MyRoomActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.app_datn_haven_inn.R
@@ -14,6 +15,7 @@ import com.example.app_datn_haven_inn.database.service.NguoiDungService
 import com.example.app_datn_haven_inn.ui.support.SupportDialog
 import com.example.app_datn_haven_inn.utils.Constans
 import com.example.app_datn_haven_inn.utils.SharedPrefsHelper
+import com.example.app_datn_haven_inn.viewModel.HoTroViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -22,17 +24,12 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MyRoomActivity : AppCompatActivity() {
-    private lateinit var fab_support : FloatingActionButton
     private lateinit var binding: ActivityMyRoomBinding
     private lateinit var adapter: MyRoomAdapter
+    private lateinit var hotroViewModel: HoTroViewModel;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        fab_support = findViewById(R.id.fab_support)
-        fab_support.setOnClickListener {
-            val intent = Intent(this, SupportDialog::class.java)
-            startActivity(intent)
-        }
 
         // Inflate the layout using ViewBinding
         binding = ActivityMyRoomBinding.inflate(layoutInflater)
@@ -44,6 +41,19 @@ class MyRoomActivity : AppCompatActivity() {
         // Set up Back Button
         binding.icBackMr.setOnClickListener {
             onBackPressed() // Handles the back navigation
+        }
+        
+        hotroViewModel = ViewModelProvider(this).get(HoTroViewModel::class.java)
+        
+        val userId = SharedPrefsHelper.getIdNguoiDung(this)
+        
+        // Xử lý khi bấm FAB
+        binding.fabSupport.setOnClickListener {
+            if (userId != null) {
+                SupportDialog(this, hotroViewModel, userId) // Tạo và hiển thị dialog
+            } else {
+                Toast.makeText(this, "Vui lòng đăng nhập để gửi hỗ trợ!", Toast.LENGTH_SHORT).show()
+            }
         }
 
         // Fetch Room Data
